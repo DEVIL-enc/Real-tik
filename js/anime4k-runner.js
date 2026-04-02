a533002ccaca31b92a3c557df0928fcf24d715f0
DEVIL-enc <moahmedjujuj@gmail.com>
2026-04-02 13:02:31 +0200

    Update anime4k-runner.js

--- js/anime4k-runner.js
+++ js/anime4k-runner.js
@@ -1,16 +1,16 @@
 async function runAnime4K(inputFile) {
 
-    console.log("Anime4K Ultra enhancement started");
+    console.log("Anime4K balanced enhancement started");
 
     await ffmpeg.run(
         "-i", inputFile,
         "-vf",
-        "scale=iw*1.2:ih*1.2:flags=lanczos,unsharp=5:5:1.0:3:3:0.5",
+        "scale=iw*1.05:ih*1.05:flags=lanczos,unsharp=3:3:0.4:2:2:0.2",
         "anime4k_temp.mp4"
     );
 
     ffmpeg.FS("rename","anime4k_temp.mp4",inputFile);
 
-    console.log("Anime4K enhancement finished");
+    console.log("Anime4K balanced enhancement finished");
 
 }
